d21d4651fc4fa80f1e73525ae50333e487b3382c
catalindrogoman <134475235+catalindrogoman@users.noreply.github.com>
2024-03-28 23:15:27 +0200

    Integrate business layers
    Add layers for business model

---
+++ handmadeShop/handmadeShop.BusinessLogic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handmadeShop.BusinessLogic
+{
+    public class Class1
+    {
+    }
+}
