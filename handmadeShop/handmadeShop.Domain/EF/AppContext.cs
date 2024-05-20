670f16a5f8d2d692efee99ce30bcac34fbfc281f
Dinu16 <grozadinu7@gmail.com>
2024-05-20 21:21:19 +0300

    added references
    added references

---
+++ handmadeShop/handmadeShop.Domain/EF/AppContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handmadeShop.Domain.EF
+{
+    public class AppContext : IdentityDbContext<ApplicationUser>
+    {
+
+        public DbSet<DeliveryCost> DeliveryCosts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<OrderItem> orderItems { get; set; }
+        public DbSet<ClientProfile> ClientProfiles { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<ReservationTable> Reservations { get; set; }
+
+        public AppContext() : base() { }
+        public static AppContext Create()
+        {
+            return new AppContext();
+        }
+
+    }
+}
