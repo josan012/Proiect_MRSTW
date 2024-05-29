c2d7fea049446bc883d8e67c2bd2086d9a1b9e65
Josan Mihai <mihai.josan12@gmail.com>
2024-05-29 18:27:59 +0300

    added some interfaces

---
+++ handmadeShop/handmadeShop.Domain/Identity/ApplicationRoleManager.cs
@@ -0,0 +1,16 @@
+﻿using handmadeShop.Domain.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handmadeShop.Domain.Identity
+{
+     public class ApplicationRoleManager : RoleManager<ApplicationRole>
+     {
+          public ApplicationRoleManager(RoleStore<ApplicationRole> store) : base(store) { }
+     }
+}
