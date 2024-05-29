f5d36d4485527ec2c5e7c00c63875388886bfc68
Josan Mihai <mihai.josan12@gmail.com>
2024-05-29 17:46:35 +0300

    added files

---
+++ handmadeShop/handmadeShop.Domain/Entities/Discount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handmadeShop.Domain.Entities
+{
+     public class Discount
+     {
+          public int Id { get; set; }
+          public decimal Percentage { get; set; }
+          public DateTime SetTime { get; set; }
+          public DateTime ExpirationTime { get; set; }
+     }
+}
