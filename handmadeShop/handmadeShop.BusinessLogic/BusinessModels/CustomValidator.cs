cd3c162c113c24d62a8c5ce4097f599f2be236a1
Dinu16 <grozadinu7@gmail.com>
2024-05-30 07:46:20 +0300

    added CustomValidator logic

--- handmadeShop/handmadeShop.BusinessLogic/BusinessModels/CustomValidator.cs
+++ handmadeShop/handmadeShop.BusinessLogic/BusinessModels/CustomValidator.cs
@@ -3,15 +3,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace handmadeShop.BusinessLogic.BusinessModels
 {
     public class CustomPasswordValidator : IIdentityValidator<string>
     {
+        public int RequiredLength { get; set; }
+        public CustomPasswordValidator(int length)
+        {
+            RequiredLength = length;
+
+        }
         public Task<IdentityResult> ValidateAsync(string item)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(item) || item.Length < RequiredLength)
+                return Task.FromResult(IdentityResult.Failed(String.Format("Minimal length is equal with: {0}", RequiredLength)));
+
+            string pattern = "^[0-9]+$";
+            if (!Regex.IsMatch(item, pattern))
+            {
+                return Task.FromResult(IdentityResult.Failed("Password should contain only digits!"));
+            }
+            return Task.FromResult(IdentityResult.Success);
         }
     }
 }
