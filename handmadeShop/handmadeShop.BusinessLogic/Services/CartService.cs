9dfaa0c416e58f92202559359ac92a134f851202
Josan Mihai <mihai.josan12@gmail.com>
2024-05-30 08:46:26 +0300

    Added interfaces and services

---
+++ handmadeShop/handmadeShop.BusinessLogic/Services/CartService.cs
@@ -0,0 +1,217 @@
+﻿using AutoMapper;
+using handmadeShop.BusinessLogic.BusinessModels;
+using handmadeShop.BusinessLogic.DTO;
+using handmadeShop.BusinessLogic.Infrastructure;
+using handmadeShop.BusinessLogic.Interfaces;
+using handmadeShop.Domain.Entities;
+using handmadeShop.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace handmadeShop.BusinessLogic.Services
+{
+     public class CartService : ICartService
+     {
+
+          private IUnitOfWork DataBase { get; set; }
+          public CartService(IUnitOfWork uow) { DataBase = uow; }
+
+          public void AddToCart(int ProductId, string category)
+          {
+               var products = DataBase.Products.Get(ProductId, category);
+               var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Product, ProductDTO>()).CreateMapper();
+               var product = mapper.Map<Product, ProductDTO>(products);
+
+               List<Item> cart = GetCart();
+
+
+
+               Item newItem = new Item { Product = product, Quantity = 1 };
+
+               int index = IsInCart(cart, ProductId);
+               if (index != -1)
+               {
+                    cart[index].Quantity++;
+               }
+               else
+               {
+
+                    cart.Add(newItem);
+               }
+
+               UpdateCart(cart);
+
+          }
+          private void UpdateCart(List<Item> cart)
+          {
+               HttpContext.Current.Session["cart"] = cart;
+          }
+          private int IsInCart(List<Item> cart, int ProductId)
+          {
+               for (int i = 0; i < cart.Count; i++)
+               {
+                    if (cart[i].Product.Id == ProductId)
+                    {
+                         return i;
+                    }
+               }
+               return -1;
+          }
+          public void ClearSession()
+          {
+
+               HttpContext.Current.Session.Clear();
+          }
+          public void RemoveFromTheCart(int ProductId)
+          {
+               List<Item> cart = GetCart();
+               int index = IsInCart(cart, ProductId);
+               if (cart[index].Quantity > 1)
+               {
+
+                    cart[index].Quantity--;
+               }
+               else
+               {
+                    cart.RemoveAt(index);
+               }
+               UpdateCart(cart);
+          }
+
+          public void Dispose()
+          {
+               DataBase.Dispose();
+          }
+
+          public List<Item> GetCart()
+          {
+               if (HttpContext.Current.Session["cart"] == null)
+               {
+                    HttpContext.Current.Session["cart"] = new List<Item>();
+               }
+               return (List<Item>)HttpContext.Current.Session["cart"];
+          }
+
+          public IEnumerable<ProductDTO> GetProducts(string category)
+          {
+               var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Product, ProductDTO>()).CreateMapper();
+               var products = DataBase.Products.GetAll(category);
+               return mapper.Map<IEnumerable<Product>, List<ProductDTO>>(products);
+          }
+
+          public ProductDTO GetProduct(int? id, string category)
+          {
+               if (id == null) throw new ValidationException("The ID was not found!", "");
+               var product = DataBase.Products.Get(id.Value, category);
+               if (product == null) throw new ValidationException("The Coffee was not found", "");
+
+               return new ProductDTO { Id = product.Id, Name = product.Name, Price = product.Price, Category = category, PathImage = product.PathImage };
+          }
+          public IEnumerable<ProductDTO> RetrieveAllProducts()
+          {
+               var products = DataBase.Products.RetrieveAllProducts();
+               var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Product, ProductDTO>()).CreateMapper();
+               var productsDTO = mapper.Map<IEnumerable<Product>, IEnumerable<ProductDTO>>(products);
+               return productsDTO;
+          }
+          public void SetDiscount(DiscountDTO discountDto)
+          {
+               var mapper = new MapperConfiguration(cfg => cfg.CreateMap<DiscountDTO, Discount>()).CreateMapper();
+               var discount = mapper.Map<DiscountDTO, Discount>(discountDto);
+               var allDiscounts = DataBase.Discounts.GetAll(null).LastOrDefault();
+               if (allDiscounts == null)
+               {
+                    DataBase.Discounts.Create(discount, null);
+                    DataBase.Save();
+               }
+               else
+               {
+                    var allDisc = new Discount
+                    {
+                         Id = allDiscounts.Id,
+                         ExpirationTime = discount.ExpirationTime,
+                         Percentage = discount.Percentage,
+                         SetTime = discount.SetTime,
+                    };
+                    DataBase.Discounts.Update(allDisc);
+                    DataBase.Save();
+               }
+
+
+          }
+
+          public IEnumerable<DiscountDTO> GetAllDiscounts()
+          {
+               var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Discount, DiscountDTO>()).CreateMapper();
+               var discounts = DataBase.Discounts.GetAll(null);
+               var discountsDTO = mapper.Map<IEnumerable<Discount>, List<DiscountDTO>>(discounts);
+               return discountsDTO;
+          }
+          public decimal CalculateTotalPrice()
+          {
+               var cartItems = GetCart();
+               var discountDTO = GetAllDiscounts().LastOrDefault();
+               var deliveryDto = GetAllDeliveriesCost().LastOrDefault();
+               var totalPrice = cartItems.Sum(x => x.Quantity * x.Product.Price);
+
+
+               if (discountDTO != null && discountDTO.ExpirationTime > DateTime.Now)
+               {
+                    var discountPercentage = discountDTO.Percentage;
+                    var discountAmount = (totalPrice * discountPercentage) / 100;
+                    totalPrice -= discountAmount;
+               }
+
+               totalPrice += deliveryDto?.Cost ?? 0;
+
+               return totalPrice;
+          }
+
+
+          public void SetDelivery(DeliveryCostDTO deliveryCostDTO)
+          {
+               var mapper = new MapperConfiguration(cfg => cfg.CreateMap<DeliveryCostDTO, DeliveryCost>()).CreateMapper();
+               var delivery = mapper.Map<DeliveryCostDTO, DeliveryCost>(deliveryCostDTO);
+               var allDelivery = DataBase.DeliveryCosts.GetAll(null).LastOrDefault();
+               if (allDelivery == null)
+               {
+                    DataBase.DeliveryCosts.Create(delivery, null);
+                    DataBase.Save();
+               }
+               else
+               {
+                    var allDel = new DeliveryCost
+                    {
+                         Id = allDelivery.Id,
+                         Cost = delivery.Cost,
+                    };
+                    DataBase.DeliveryCosts.Update(allDel);
+                    DataBase.Save();
+               }
+
+          }
+          public IEnumerable<DeliveryCostDTO> GetAllDeliveriesCost()
+          {
+               var mapper = new MapperConfiguration(cfg => cfg.CreateMap<DeliveryCost, DeliveryCostDTO>()).CreateMapper();
+               var deliveries = DataBase.DeliveryCosts.GetAll(null);
+               var deliveriesDTO = mapper.Map<IEnumerable<DeliveryCost>, List<DeliveryCostDTO>>(deliveries);
+               return deliveriesDTO;
+          }
+
+          public bool RemoveDiscount()
+          {
+               var discount = GetAllDiscounts().LastOrDefault();
+               if (discount != null)
+               {
+                    DataBase.Discounts.Delete(discount.Id, null);
+                    DataBase.Save();
+                    return true;
+               }
+               return false;
+          }
+     }
+}
---
+++ handmadeShop/handmadeShop.BusinessLogic/Services/OrderService.cs
@@ -0,0 +1,139 @@
+﻿using AutoMapper;
+using handmadeShop.BusinessLogic.BusinessModels;
+using handmadeShop.BusinessLogic.DTO;
+using handmadeShop.BusinessLogic.Infrastructure;
+using handmadeShop.BusinessLogic.Interfaces;
+using handmadeShop.Domain.Entities;
+using handmadeShop.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace handmadeShop.BusinessLogic.Services
+{
+     public class OrderService : IOrderService
+     {
+          IUnitOfWork DataBase { get; set; }
+          public OrderService(IUnitOfWork uow) { DataBase = uow; }
+          public void MakeOrder(OrderDTO orderDTO)
+          {
+               var cartItems = (List<Item>)HttpContext.Current.Session["cart"];
+
+
+
+               Order order = new Order
+               {
+                    FirstName = orderDTO.FirstName,
+                    LastName = orderDTO.LastName,
+                    Appartment = orderDTO.Appartment,
+                    StreetAddress = orderDTO.StreetAddress,
+                    Phone = orderDTO.Phone,
+                    City = orderDTO.City,
+                    Country = orderDTO.Country,
+                    PostCode = orderDTO.PostCode,
+                    Email = orderDTO.Email,
+                    TotalSumToPay = orderDTO.TotalSumToPay,
+                    BuyingTime = DateTime.Now,
+                    ApplicationUserId = orderDTO.ApplicationUserId
+               };
+
+               foreach (var cartItem in cartItems)
+               {
+                    var orderItem = new OrderItem
+                    {
+                         ProductId = cartItem.Product.Id, // Associate existing Product with OrderItem
+                         Order = order,
+                         Quantity = cartItem.Quantity,
+
+                    };
+
+                    order.Items.Add(orderItem);
+               }
+               DataBase.Orders.Create(order, "");
+               DataBase.Save();
+          }
+          public ProductDTO GetProductWithouCategory(int productId)
+          {
+
+               var product = DataBase.Products.GetProduct(productId);
+               if (product == null) throw new ValidationException($"The {product.Name} was not found", "");
+               return new ProductDTO { Id = product.Id, Name = product.Name, Price = product.Price, PathImage = product.PathImage };
+          }
+          public ProductDTO GetProduct(int? id, string category)
+          {
+               if (id == null) throw new ValidationException("The ID was not found!", "");
+               var product = DataBase.Products.Get(id.Value, category);
+               if (product == null) throw new ValidationException($"The {product.Name} was not found", "");
+
+               return new ProductDTO { Id = product.Id, Name = product.Name, Price = product.Price, Category = category };
+          }
+
+          public decimal CalculateTotalSum(OrderDTO orderDTO, string category)
+          {
+               decimal totalSum = 0;
+               var products = DataBase.Products.GetAll(category);
+               foreach (var productId in products)
+               {
+                    ProductDTO product = GetProduct(orderDTO.Id, category);
+                    totalSum += product.Price;
+               }
+
+               return totalSum;
+          }
+
+          public bool DeleteOrdersByUserId(string userId)
+          {
+               try
+               {
+                    var orders = DataBase.Orders.GetOrdersByUserId(userId);
+                    if (orders != null)
+                    {
+                         foreach (var order in orders)
+                         {
+                              DataBase.Orders.Delete(order.Id, null);
+                         }
+                         return true;
+                    }
+                    return false;
+               }
+               catch (Exception ex)
+               {
+                    throw new Exception(ex.Message);
+
+               }
+          }
+          public void Dispose()
+          {
+               DataBase.Dispose();
+          }
+
+          public IEnumerable<OrderDTO> GetOrders(string category)
+          {
+               var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Order, OrderDTO>()).CreateMapper();
+               return mapper.Map<IEnumerable<Order>, List<OrderDTO>>(DataBase.Orders.GetAll(category));
+          }
+
+          public IEnumerable<ProductDTO> GetProducts(string category)
+          {
+               var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Product, ProductDTO>()).CreateMapper();
+               return mapper.Map<IEnumerable<Product>, List<ProductDTO>>(DataBase.Products.GetAll(category));
+          }
+
+          public OrderDTO GetOrder(int id)
+          {
+               var order = DataBase.Orders.Get(id, "");
+               var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Order, OrderDTO>()).CreateMapper();
+               return mapper.Map<Order, OrderDTO>(order);
+          }
+
+          public IEnumerable<OrderDTO> GetOrdersByUserId(string id)
+          {
+               var orders = DataBase.Orders.GetOrdersByUserId(id);
+               var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Order, OrderDTO>()).CreateMapper();
+               return mapper.Map<IEnumerable<Order>, List<OrderDTO>>(orders);
+          }
+     }
+}
