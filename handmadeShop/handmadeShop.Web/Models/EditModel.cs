f57990edcd66edcf68883e8e29228e303bef64e6
catalindrogoman <134475235+catalindrogoman@users.noreply.github.com>
2024-05-30 21:43:10 +0300

    Add .js scripts, models, controllers

---
+++ handmadeShop/handmadeShop.Web/Models/EditModel.cs
@@ -0,0 +1,11 @@
+﻿namespace handmadeShop.Web.Models
+{
+    public class EditModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ handmadeShop/handmadeShop.Web/jquery_scripts/ItemCount.js
@@ -0,0 +1,4 @@
+export function updateCartItemCount(count) {
+    document.getElementById("cart-item-count").textContent = count;
+}
+
