04b980284bc707b81d3022d87716119701514c65
Dinu16 <grozadinu7@gmail.com>
2024-05-30 10:21:35 +0300

    moved DTO from BusinessModels to DTO folder, added ServiceModule and interface: IUserInterface

---
+++ handmadeShop/handmadeShop.BusinessLogic/Infrastructure/ServiceModule.cs
@@ -0,0 +1,24 @@
+﻿using handmadeShop.Domain.Interfaces;
+using handmadeShop.Domain.Repositories;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handmadeShop.BusinessLogic.Infrastructure
+{
+    public class ServiceModule : NinjectModule
+    {
+        private string connectionString;
+        public ServiceModule(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<EFUnitOfWork>().WithConstructorArgument(connectionString);
+        }
+    }
+}
