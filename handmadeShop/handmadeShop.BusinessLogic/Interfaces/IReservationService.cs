b987ed6f46e0dc9e7fc83c6fab77e61e56ba51a7
Dinu16 <grozadinu7@gmail.com>
2024-05-30 10:35:16 +0300

    added two interfaces: IReservationService and IUserService

---
+++ handmadeShop/handmadeShop.BusinessLogic/Interfaces/IReservationService.cs
@@ -0,0 +1,19 @@
+﻿using handmadeShop.BusinessLogic.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handmadeShop.BusinessLogic.Interfaces
+{
+    public interface IReservationService
+    {
+        void MakeReservation(ReservationTableDTO reservationDTO);
+        ReservationTableDTO GetReservation(int id);
+        IEnumerable<ReservationTableDTO> GetReservations();
+        IEnumerable<ReservationTableDTO> GetReservationsByUserId(string userId);
+        void DeleteReservation(int reservationId);
+        void Dispose();
+    }
+}
