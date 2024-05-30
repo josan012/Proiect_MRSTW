2c11b8225e2d109cebc56adc6c6dc81613294245
Dinu16 <grozadinu7@gmail.com>
2024-05-30 10:45:03 +0300

    added two services: ReservationService and ServiceCreator

---
+++ handmadeShop/handmadeShop.BusinessLogic/Services/ServiceCreator.cs
@@ -0,0 +1,18 @@
+﻿using handmadeShop.BusinessLogic.Interfaces;
+using handmadeShop.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handmadeShop.BusinessLogic.Services
+{
+    public class ServiceCreator : IServiceCreator
+    {
+        public IUserService CreateUserService()
+        {
+            return new UserService(new EFUnitOfWork());
+        }
+    }
+}
